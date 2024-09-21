734310076de911fda6c7beb0698c950ecfdde3e8
Dimitri Vavoulis <dimitrivavoulis3@Gmail.com>
2024-09-21 09:22:27 -0400

    Update production-testing-sentiment-analysis-script.py

--- 8-Testing converted model/production-testing-sentiment-analysis-script.py
+++ 8-Testing converted model/production-testing-sentiment-analysis-script.py
@@ -96,3 +96,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# This file contains code that uses the "Yelp Reviews for SA fine-grained 5 classes CSV" dataset,
+# which is licensed under the Apache License 2.0. 
+# For more information, see: https://www.kaggle.com/datasets/yacharki/yelp-reviews-for-sa-finegrained-5-classes-csv
